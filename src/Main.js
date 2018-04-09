c139b21a6b00fee3775dc0d75438377e4342b933
Frank Bassard <frank.bassard@springload.co.nz>
2018-04-09 17:07:32 +1200

    Anim import

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group'
 import colours from './colours';
 import fonts from './fonts';
 import './Main.css';
 
-const input = ['This is a header And this is a paragraph', 'tete'];
+const input = ['quote L1', 'quote L2'];
 
 class Main extends Component {
   render() {
@@ -12,21 +13,26 @@ class Main extends Component {
     const [ backgroundColor, color ] = colours[crdm];
     const style = { backgroundColor, color };
     return (
-      <div className={'main ' + fonts[frdm]} style={style}>
-        <div className='topbar'>
-          <span className='left'>
-            <i className="material-icons arrow">arrow_back</i>
-            <span>Previous</span>
-          </span>
-          <span className='right'>
-            <span>Next</span>
-            <i className="material-icons arrow">arrow_forward</i>
-          </span>
+      <CSSTransitionGroup
+        transitionName="example"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        <div className={'main ' + fonts[frdm]} style={style}>
+            <div className='topbar'>
+              <span className='left'>
+                <i className="material-icons arrow">arrow_back</i>
+                <span>Previous</span>
+              </span>
+              <span className='right'>
+                <span>Next</span>
+                <i className="material-icons arrow">arrow_forward</i>
+              </span>
+            </div>
+            <div className='content'>
+              {input}
+            </div>
         </div>
-        <div className='content'>
-          {input}
-        </div>
-      </div>
+      </CSSTransitionGroup>
     );
   }
 }
